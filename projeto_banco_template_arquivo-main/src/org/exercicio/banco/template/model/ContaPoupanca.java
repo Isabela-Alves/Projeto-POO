069f60d9efac26eefe05fe976d949e386cd7df11
Isabela da Silva Alves <108693206+Isabela-Alves@users.noreply.github.com>
2023-06-14 09:32:56 -0300

    Update ContaPoupanca.java

--- projeto_banco_template_arquivo-main/src/org/exercicio/banco/template/model/ContaPoupanca.java
+++ projeto_banco_template_arquivo-main/src/org/exercicio/banco/template/model/ContaPoupanca.java
@@ -10,7 +10,7 @@ import java.util.Objects;
 import java.util.Random;
 
 import org.exercicio.banco.template.model.enumerator.TipoTransacao;
-import org.exercicio.banco.template.persistence.PersistenciaEmArquivo;
+
 
 
 public class ContaPoupanca implements Serializable, IConta {
@@ -80,13 +80,13 @@ public class ContaPoupanca implements Serializable, IConta {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		ContaCorrente other = (ContaCorrente) obj;
+		ContaPoupanca other = (ContaPoupanca) obj;
 		return Objects.equals(numeroConta, other.getNumeroConta());
 	}
 
 	@Override
 	public String toString() {
-		return "ContaBancaria [numeroConta=" + numeroConta + ", saldo=" + saldo + ", dataAbertura=" + dataAbertura
+		return "Conta Poupança [numeroConta=" + numeroConta + ", saldo=" + saldo + ", dataAbertura=" + dataAbertura
 				+ ", status=" + status + "]";
 	}
 
@@ -127,21 +127,32 @@ public class ContaPoupanca implements Serializable, IConta {
 	}
 
 	public void transferir(IConta c, BigDecimal quantia) {
+		BigDecimal taxa = BigDecimal.ZERO;
+		BigDecimal taxaPorcentagem = new BigDecimal("0.2");
 		if (status && c.isStatus()) {
 			if (quantia.compareTo(BigDecimal.ZERO) < 0) {
 				System.err.println("Valor invalido para transferencia.");
-			} else if (quantia.compareTo(saldo) <= 0) {
-				setSaldo(saldo.subtract(quantia));
+			} else {
+			   if (c instanceof ContaCorrente) {
+				  taxa = quantia.multiply(taxaPorcentagem);
+			
+			} if (quantia.compareTo(saldo.add(taxa)) <= 0) {
+				setSaldo(saldo.subtract(quantia).subtract(taxa));
 				c.setSaldo(c.getSaldo().add(quantia));
 				c.getTransacoes().add(new RegistroTransacao(quantia, TipoTransacao.TRANSACAO_CREDITO, LocalDateTime.now()));
 				transacoes.add(new RegistroTransacao(quantia, TipoTransacao.TRANSACAO_DEBITO, LocalDateTime.now()));
 				System.out.println("Transferência realizada com sucesso.");
-			} else
+			 
+			} else {
 				System.err.println("Saldo insuficiente para realizar a transferencia.");
-		} else {
-			System.err.println("Operacao nao pode ser realizada entre contas desativadas.");
+			 }
 		}
-
+	
+	} else {
+			System.err.println("Operacao nao pode ser realizada entre contas desativadas.");
+		
+		}
+		
 	}
 	
 	public void imprimirExtratoConta(int mes, int year) {
